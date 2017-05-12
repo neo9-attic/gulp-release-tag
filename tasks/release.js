fc27d542ece731f2a8fc42a3b66e99f11e97b0da
Gaetan SENN <gaetan.senn@gmail.com>
2017-05-12 16:58:37 +0200

    script: Add bump path parameter

--- tasks/release.js
+++ tasks/release.js
@@ -1,58 +1,63 @@
 module.exports = function (gulp) {
-	var fs = require('fs');
-	var gutil = require('gulp-util');
-	var git = require('gulp-git');
-	var	bump = require('gulp-bump');
-	var runSequence = require('run-sequence').use(gulp);
+  var fs = require('fs');
+  var gutil = require('gulp-util');
+  var git = require('gulp-git');
+  var bump = require('gulp-bump');
+  var path = require('path');
+  var runSequence = require('run-sequence').use(gulp);
 
-	function getPackageJsonVersion () {
-		return JSON.parse(fs.readFileSync('./package.json', 'utf8')).version;
-	}
+  function getPackageJsonVersion() {
+    return JSON.parse(fs.readFileSync('./package.json', 'utf8')).version;
+  }
 
-	gulp.task('bump-version', function () {
-		var argv = require('minimist')(process.argv.slice(2));
-		var type = argv['t'] || 'patch';
+  gulp.task('bump-version', function () {
+    var argv = require('minimist')(process.argv.slice(2));
+    var type = argv['t'] || 'patch';
+    var directory = argv['p'] || './';
 
-		return gulp.src(['./bower.json', './package.json'])
-			.pipe(bump({type: type}).on('error', gutil.log))
-			.pipe(gulp.dest('./'));
-	});
+    console.log([path.join(directory, 'bower.json'), path.join(directory, 'package.json')]);
 
-	gulp.task('commit-changes', function () {
-		var version = getPackageJsonVersion();
+    return gulp.src([path.join(directory, 'bower.json'), path.join(directory, 'package.json')])
+      .pipe(bump({ type: type }).on('error', gutil.log))
+      .pipe(gulp.dest(directory));
+  });
 
-		return gulp.src('.')
-			.pipe(git.add())
-			.pipe(git.commit('release: Version ' + version));
-	});
+  gulp.task('commit-changes', function () {
+    z
+    var version = getPackageJsonVersion();
 
-	gulp.task('push-changes', function (next) {
-		var argv = require('minimist')(process.argv.slice(2));
-		var branch = argv['b'] || 'master';
+    return gulp.src('.')
+      .pipe(git.add())
+      .pipe(git.commit('release: Version ' + version));
+  });
 
-		git.push('origin', branch, next);
-	});
+  gulp.task('push-changes', function (next) {
+    var argv = require('minimist')(process.argv.slice(2));
+    var branch = argv['b'] || 'master';
 
-	gulp.task('create-new-tag', function (next) {
-		var version = getPackageJsonVersion();
+    git.push('origin', branch, next);
+  });
 
-		git.tag('v' + version, 'v' + version, function (err) {
-			if (err) return next(err);
+  gulp.task('create-new-tag', function (next) {
+    var version = getPackageJsonVersion();
 
-			git.push('origin', 'v' + version, next);
-		});
-	});
+    git.tag('v' + version, 'v' + version, function (err) {
+      if (err) return next(err);
 
-	gulp.task('release', function (next) {
-		runSequence(
-			'bump-version',
-			'commit-changes',
-			'push-changes',
-			'create-new-tag',
-			function (err) {
-				if (err) console.log(err.message);
+      git.push('origin', 'v' + version, next);
+    });
+  });
 
-				next(err);
-			});
-	});
+  gulp.task('release', function (next) {
+    runSequence(
+      'bump-version',
+      'commit-changes',
+      'push-changes',
+      'create-new-tag',
+      function (err) {
+        if (err) console.log(err.message);
+
+        next(err);
+      });
+  });
 };
